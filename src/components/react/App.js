f8a3bffb7b11cb83c9aed6de967656f3ed77ab2f
saransh-283 <saranshmm@gmail.com>
2021-05-09 23:56:49 +0530

    Styling II

--- src/components/react/App.js
+++ src/components/react/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import '../../assets/css/mobile-react.css'
 import React from 'react';
-import quotesList from '../js/quotes'
+import quotesList from '../../assets/js/quotes'
 
 class Card extends React.Component {
   constructor(props) {
@@ -21,12 +22,13 @@ class Card extends React.Component {
             <p className="author">- {this.state.author}</p>
           </blockquote>
           <footer id="footer">
-            <div>
-              <a href="#" className="btn social-btn"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-              <a href="#" className="btn social-btn"><i className="fa fa-tumblr" aria-hidden="true"></i></a>
+            <div id="social-btns">
+              <a href="#" className="btn social-btn" id="twitter"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+              <a href="#" className="btn social-btn" id="tumblr"><i className="fa fa-tumblr" aria-hidden="true"></i></a>
               </div>
-            <div>
-              <a href="#" className="btn" id="new-quote" onClick={this.changeQuote}>New Quote</a>
+            <div id="new-btns">
+              <a href="#" className="btn btn-light new-btn"  onClick={this.changeQuote}>New Quote</a>
+              <a href="#" className="btn btn-light new-btn"  id="new-wallpaper">New Wallpaper</a>
             </div>
           </footer>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/react/App';
-import quotesList from './components/js/quotes'
+import quotesList from './assets/js/quotes'
 import bg0 from '../src/assets/img/0.jpg'
 import bg1 from '../src/assets/img/1.jpg'
 import bg2 from '../src/assets/img/2.jpg'
 import bg3 from '../src/assets/img/3.jpg'
-import bg4 from '../src/assets/img/4.jpg'
 
+function randomWallpaper() {
+    var bgRandom = Math.floor(Math.random() * 4)
+    var bgArr = [bg0, bg1, bg2, bg3]
+    document.body.style.backgroundImage = 'url(' + bgArr[bgRandom] + ')'
+    console.log('hi')
+}
 
-var bgRandom = Math.floor(Math.random() * 5)
-var bgArr = [bg0, bg1, bg2, bg3, bg4]
-document.body.style.backgroundImage = 'url(' + bgArr[bgRandom] + ')'
+randomWallpaper()
 
 var randomQuote = Math.floor(Math.random() * (quotesList.length))
 
-ReactDOM.render( < Card
-        default = {
-            quotesList[randomQuote]
-        }
-        / > , document.getElementById('container'))
+ReactDOM.render(< Card default={
+    quotesList[randomQuote]
+} />, document.getElementById('container'))
+
+document.getElementById('new-wallpaper').onclick=function(){randomWallpaper()}
